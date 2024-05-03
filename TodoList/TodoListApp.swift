0c75f327c7c1d315ee99327f1969b647492d341a
Emirhan Zeyrek <emirhanzeyrekcom@gmail.com>
2024-05-03 23:05:13 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Emirhan Zeyrek on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
